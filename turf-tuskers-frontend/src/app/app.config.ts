50fe7e7885406d7e0b3bad39f12f272fa3321619
Prem Kumar R <premramaiah04@gmail.com>
2025-05-03 16:33:48 +0530

    TurfTuskersFrontend: Tailwindcss installations and basic components setup done.

--- turf-tuskers-frontend/src/app/app.config.ts
+++ turf-tuskers-frontend/src/app/app.config.ts
@@ -7,3 +7,7 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
 };
+
+export default {
+  plugins: ["@tailwindcss/postcss"],
+};
