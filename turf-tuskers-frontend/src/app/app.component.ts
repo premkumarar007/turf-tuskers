abe0fa29ab8d3fb49be5bd516ec87749e2a8b0f5
Prem Kumar R <premramaiah04@gmail.com>
2025-05-03 17:20:36 +0530

    TurfTuskersFrontend: routing setup of all components done.

--- turf-tuskers-frontend/src/app/app.component.ts
+++ turf-tuskers-frontend/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from "./components/home-page/home-page.component";
+import { MatchDetailsComponent } from "./components/match-details/match-details.component";
+import { AdminDashboardComponent } from "./components/admin-dashboard/admin-dashboard.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HomePageComponent, MatchDetailsComponent, AdminDashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- turf-tuskers-frontend/src/app/app.routes.ts
+++ turf-tuskers-frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { MatchDetailsComponent } from './components/match-details/match-details.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomePageComponent},
+    {path: 'match/:id', component: MatchDetailsComponent},
+    {path: 'admin', component: AdminDashboardComponent},
+];
